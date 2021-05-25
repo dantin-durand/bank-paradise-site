81981aa074119b792988425ccca754dec17807db
Dantin Durand <durand.dantin.pro@gmail.com>
2021-05-25 12:52:20 +0200

    ajout des routes et des view login/register

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Un titre d’environ 70 caractères</title>
+    <meta name="description" content="Une phrase d’environ 170 caractères">
+    <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <h1>Dashboard</h1>
+</body>
+
+</html>
