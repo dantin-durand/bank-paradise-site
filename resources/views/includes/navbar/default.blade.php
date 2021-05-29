3346c7e5a48bc5d4868625087c86a66343b36c30
Dantin Durand <durand.dantin.pro@gmail.com>
2021-05-29 17:16:22 +0200

    modification des link navbar

--- resources/views/includes/navbar/default.blade.php
+++ resources/views/includes/navbar/default.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar-default__container">
-    <img src="/img/logo-white.svg" alt="logo bank-paradise">
+    <a href="/"><img src="/img/logo-white.svg" alt="logo bank-paradise"></a>
     <div>
         <div class="navbar__burger-menu">
             <span></span>
@@ -8,13 +8,13 @@
         </div>
         <div class="navbar__items">
             <ul>
-                <li><a href="/">Services</a></li>
-                <li><a href="/">News</a></li>
-                <li><a href="/">Contact</a></li>
+                <li><a href="/services">Services</a></li>
+                <li><a href="/news">News</a></li>
+                <li><a href="/contact">Contact</a></li>
             </ul>
             <ul>
-                <li><a href="/">Conexion</a></li>
-                <li><a href="/">Inscription</a></li>
+                <li><a href="{{ route('login') }}">Conexion</a></li>
+                <li><a href="{{ route('register') }}">Inscription</a></li>
             </ul>
         </div>
     </div>
