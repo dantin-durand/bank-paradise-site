6875dc2fa01eaf9e8e2a61a1bc466fb323f9b0d3
Dantin Durand <durand.dantin.pro@gmail.com>
2021-05-30 19:54:54 +0200

    mise en place des differents boutons

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <meta name="viewport" content="height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/navbar/default.css') }}">
+</head>
+
+<body class="home__container">
+    @include('includes.navbar.default')
+    @yield('content')
+
+    <script src="{{ asset('js/navbar.js') }}"></script>
+</body>
+
+</html>
