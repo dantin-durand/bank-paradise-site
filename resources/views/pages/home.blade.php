5fe4d626a0aea1e09cb29d70f664bb3204cd64c9
Dantin Durand <durand.dantin.pro@gmail.com>
2021-06-01 08:52:41 +0200

    mise en place des formules sur la home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,68 @@
     <a href="/services" class="btn btn-large">Découvrir</a>
 </header>
 <main>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, quam? A, corporis ratione recusandae atque libero nesciunt quis obcaecati suscipit fuga pariatur officiis illum autem non sapiente, ipsa ducimus! Nihil blanditiis amet quod sint non similique architecto vitae, eius inventore nesciunt maiores sequi nulla doloremque reiciendis quibusdam suscipit natus harum adipisci, facilis eligendi a ratione nobis. Laborum, repellat fugit? Est voluptatem dolor quam harum iste quidem consectetur incidunt nemo vel obcaecati consequuntur optio eligendi ipsa ex, veniam eius perspiciatis error animi dicta necessitatibus illum! Quidem eligendi odio ipsa molestiae voluptatibus, aperiam molestias suscipit fuga id eveniet culpa, libero accusamus quae!
-    </p>
+    <section class="services__section">
+        <img src="/img/steps/transactions.svg" alt="Transactions Rôleplay">
+        <div>
+            <h1>Transactions Rôleplay</h1>
+            <p>Effectuez des transactions intracommunautaires ou extracommunautaires avec l'offre évasion. Payez avec une monnaie en temps réel, consultez vos transactions.</p>
+            <a href="/services" class="btn btn-large btn-full">Découvrir</a>
+        </div>
+    </section>
+    <section class="services__section">
+        <img src="/img/steps/admin.svg" alt="Administration et gestion
+de la communauté">
+        <div>
+            <h1>Administration et gestion
+                de la communauté</h1>
+            <p>En tant que fondateur d'une communauté, vous avez la possibilité de contrôler et de supprimer des virements ou des membres. Vous avez également accès au registre des joueurs signalés par d'autres communautés.</p>
+            <a href="/services" class="btn btn-large">Découvrir</a>
+        </div>
+    </section>
+    <section class="formules__sections">
+        <h1 class="title t-center">Formules</h1>
+        <div class="formules__item">
+            <h1>1.99€</h1>
+            <p>Nouvel arrivant</p>
+            <div class="formule_spacer"></div>
+            <ul>
+                <li>Jusqu'à <strong>10 membres</strong></li>
+                <li>Transactions <strong>illimitées</strong></li>
+                <li>Historique des transactions</li>
+            </ul>
+            <div>
+                <a href="/services" class="btn btn-large btn-full">Sélectionner</a>
+            </div>
+        </div>
+        <div class="formules__item recommanded">
+            <h1>3.99€</h1>
+            <p>Communauté</p>
+            <div class="formule_spacer"></div>
+            <ul>
+                <li>Jusqu'à <strong>30 membres</strong></li>
+                <li>Transactions <strong>illimitées</strong></li>
+                <li>Historique des transactions</li>
+                <li><strong>3 Comptes entreprises</strong> par <strong>membre</strong></li>
+            </ul>
+            <div>
+                <a href="/services" class="btn btn-large btn-full">Sélectionner</a>
+            </div>
+        </div>
+        <div class="formules__item">
+            <h1>1.99€</h1>
+            <p>Nouvel arrivant</p>
+            <div class="formule_spacer"></div>
+            <ul>
+                <li>Membres<strong>illimités</strong></li>
+                <li>Transactions <strong>illimitées</strong></li>
+                <li>Transactions <strong>Extracommunautaires</strong></li>
+                <li>Historique des transactions</li>
+                <li>Compte entreprise <strong>illimité</strong></li>
+            </ul>
+            <div>
+                <a href="/services" class="btn btn-large btn-full">Sélectionner</a>
+            </div>
+        </div>
+    </section>
 </main>
 @endsection
--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -28,14 +28,14 @@
     </div>
 </header>
 <main class="services__main__container">
-    <section>
+    <section class="services__section">
         <img src="/img/steps/transactions.svg" alt="Transactions Rôleplay">
         <div>
             <h1>Transactions Rôleplay</h1>
             <p>Effectuez des transactions intracommunautaires ou extracommunautaires avec l'offre évasion. Payez avec une monnaie en temps réel, consultez vos transactions.</p>
         </div>
     </section>
-    <section>
+    <section class="services__section">
         <img src="/img/steps/admin.svg" alt="Administration et gestion
 de la communauté">
         <div>
@@ -44,14 +44,14 @@ de la communauté">
             <p>En tant que fondateur d'une communauté, vous avez la possibilité de contrôler et de supprimer des virements ou des membres. Vous avez également accès au registre des joueurs signalés par d'autres communautés.</p>
         </div>
     </section>
-    <section>
+    <section class="services__section">
         <img src="/img/steps/entrerpise.svg" alt="Compte entreprise">
         <div>
             <h1>Compte entreprise</h1>
             <p>Gérez les comptes bancaires de votre entreprise avec un système de gestion des employés et un paramétrage de l'envoi automatique des salaires.</p>
         </div>
     </section>
-    <section>
+    <section class="services__section">
         <img src="/img/steps/virements.svg" alt="Historique des virements">
         <div>
             <h1>Historique des virements</h1>
