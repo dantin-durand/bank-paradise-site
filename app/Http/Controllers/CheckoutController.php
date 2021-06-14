11f53d0d9f6a896cb5cac4dd5f8f97a37a0c21b1
Dantin Durand <durand.dantin.pro@gmail.com>
2021-06-14 22:24:30 +0200

    ajout des migrations stripe

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Plan;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
@@ -14,6 +15,27 @@ class CheckoutController extends Controller
     }
     public function store(Request $request)
     {
-        dd($request);
+        $request->validate([
+            'name' => 'required',
+            'payment_method' => 'required',
+            'plan' => 'required|exists:plans,id',
+            'coupon' => 'nullable'
+        ]);
+
+        $plan = Plan::find($request->plan);
+
+        try {
+            $subscription = $request
+                ->user()
+                ->newSubscription('default', $plan->stripe_id)
+                ->withCoupon($request->coupon)
+                ->create($request->payment_method);
+        } catch (\Laravel\Cashier\Exceptions\IncompletePayment $e) {
+            return redirect()->route('cashier.payment', [
+                $e->payment->id, 'redirect' => route('payments.error')
+            ]);
+        }
+
+        return view('payments.success');
     }
 }
--- resources/views/auth/register3.blade.php
+++ resources/views/auth/register3.blade.php
@@ -10,24 +10,48 @@
             <h2>3.99€<span>/mois</span></h2>
             <p>formule: <strong>Communauté</strong></p>
         </div>
-        <input type="hidden" name="formule" value="{{ app('request')->input('formule') }}">
+        <input type="hidden" name="plan" value="{{ app('request')->input('formule') }}">
         <br>
         <div class="auth__input-container">
-
+            
             <input id="name" class="input" type="text" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="{{ __('Nom') }}">
-
+            
             @error('name')
             <span role="alert">
                 <strong>{{ $message }}</strong>
             </span>
             @enderror
-
+            
         </div>
+        <br>
         <br>
         <div id="card-element">
             <!-- Elements will create input elements here -->
         </div>
         <br>
+        <br>
+        <div class="auth__input-container">
+            
+            <input id="coupon" class="input" type="text" name="coupon" value="{{ old('coupon') }}" required autocomplete="coupon" autofocus placeholder="{{ __('Coupon') }}">
+            
+            @error('coupon')
+            <span role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+            
+        </div>
+        <br>
+        <br>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
         <!-- We'll put the error messages in this element -->
         <div id="card-errors" role="alert"></div>
         <button type="submit" id="card-button" class="btn btn-large btn-full" data-secret="{{ $intent->client_secret }}">Payer</button>
@@ -40,6 +64,7 @@
 
 <script src="https://js.stripe.com/v3/"></script>
 <script>
+
     const stripe = Stripe('pk_test_51IzzVJBQlJTYhI99hHScvWlR6gJcJSrDC5ze3jd5j3E6Lkx1UwXJi1tnvZJzcZvsuIMf4oz34fVVDKt2QYQF7CCs00utOOiKVF');
     const elements = stripe.elements();
     var style = {
@@ -67,18 +92,17 @@
             displayError.textContent = '';
         }
     });
-
+// c'est gérer en js par stripe en gros ici dans tout le code js on verif si la carte est bonne avec stripe et après ça nous chie un genre de token pour le paiement et ce token on le passe à laravel dans le controller grace à la request
     var form = document.getElementById('payment-form');
 
     cardButton.addEventListener('click', async (e) => {
         e.preventDefault();
-        const {
-            paymentMethod,
-            error
-        } = await stripe.createPaymentMethod(
-            'card', cardElement, {
-                billing_details: {
-                    name: cardHolderName.value
+
+        const { setupIntent, error } = await stripe.confirmCardSetup(
+            clientSecret, {
+                payment_method: {
+                    card: cardElement,
+                    billing_details: { name: cardHolderName.value }
                 }
             }
         );
@@ -86,7 +110,13 @@
         if (error) {
             console.log(error);
         } else {
-            console.log(set);
+            console.log(setupIntent);
+            let paymentMethod = document.createElement('input');
+            paymentMethod.setAttribute('type', 'hidden');
+            paymentMethod.setAttribute('name', 'payment_method');
+            paymentMethod.value = setupIntent.payment_method;
+            form.appendChild(paymentMethod);
+            form.submit();
         }
     });
 </script>
