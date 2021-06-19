52475098e1016a8f35f1d4c33ef3a88f86b26d4d
Dantin Durand <durand.dantin.pro@gmail.com>
2021-06-19 23:58:11 +0200

    modification migration plans

--- database/migrations/2021_06_08_082437_create_plans_table.php
+++ database/migrations/2021_06_08_082437_create_plans_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreatePlansTable extends Migration
 {
@@ -20,6 +21,25 @@ class CreatePlansTable extends Migration
             $table->string('name');
             $table->string('stripe_id');
         });
+
+
+        DB::table('plans')->insert([
+            'price' => 199,
+            'name' => "Nouvel arrivant",
+            'stripe_id' => "price_1J0096BQlJTYhI99rMdPY78y"
+        ]);
+
+        DB::table('plans')->insert([
+            'price' => 399,
+            'name' => "Communauté",
+            'stripe_id' => "price_1J2H76BQlJTYhI99lMsk921x"
+        ]);
+
+        DB::table('plans')->insert([
+            'price' => 599,
+            'name' => "Évasion",
+            'stripe_id' => "price_1J2fqWBQlJTYhI99ACOHwMwV"
+        ]);
     }
 
     /**
